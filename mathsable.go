a1bde4aa344aae57aef65dc54e0abbd9ed14e91f
timlomax71 <Tim.Lomax@live.co.uk>
2016-07-26 23:10:03 +0100

    Sorting out templates and passing around the context.

--- mathsable.go
+++ mathsable.go
@@ -3,14 +3,49 @@ package main
 import (
   "net/http"
   "html/template"
+  "log"
+  "io"
+  "time"
 )
 
+const STATIC_URL string = "/www/static/"
+const STATIC_ROOT string = "www/static/"
+
+type Context struct {
+  Title string `json:"title"`
+  Static string `json:"static"`
+}
+
+var templates = template.Must(template.ParseGlob("www/templates/*"))
+
 func handler(w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("./tmpl/layout.html", "./tmpl/head.html", "./tmpl/login.html", "./tmpl/naviagtion.html")
-  t.ExecuteTemplate(w, "layout", "")
+  context := Context{Title: "Hello"}
+  render(w, "layout.html", context)
+}
+
+func render(w http.ResponseWriter, template string, context Context) {
+  context.Static = STATIC_URL
+  err := templates.ExecuteTemplate(w, template, context)
+  if err != nil {
+    log.Print("Failed to render template: ", err)
+  }
+}
+
+func StaticHandler(w http.ResponseWriter, req *http.Request) {
+  static_file := req.URL.Path[len(STATIC_URL):]
+  if len(static_file) != 0 {
+    f, err := http.Dir(STATIC_ROOT).Open(static_file)
+    if err == nil {
+      content := io.ReadSeeker(f)
+      http.ServeContent(w, req, static_file, time.Now(), content)
+      return
+    }
+  }
+  http.NotFound(w, req)
 }
 
 func main() {
   http.HandleFunc("/", handler)
+  http.HandleFunc(STATIC_URL, StaticHandler)
   http.ListenAndServe(":8081", nil)
 }

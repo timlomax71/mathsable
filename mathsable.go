96947ed0d4f2d1c6b66e4fd32ec96e3548eff1ae
timlomax71 <Tim.Lomax@live.co.uk>
2016-07-27 21:31:51 +0100

    Fixing some css and adding the login to the landing page.

--- mathsable.go
+++ mathsable.go
@@ -1,59 +1,50 @@
 package main
 
 import (
-  "net/http"
-  "html/template"
-  "log"
-  "io"
-  "time"
-  "os"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
-
-const STATIC_URL string = "/www/static/"
-const STATIC_ROOT string = "www/static/"
-
-type Context struct {
-  Title string `json:"title"`
-  Static string `json:"static"`
-}
 
 var templates = template.Must(template.ParseGlob("www/templates/*"))
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  context := Context{Title: "Hello"}
-  render(w, "layout.html", context)
+	render(w, "layout")
 }
 
-func render(w http.ResponseWriter, template string, context Context) {
-  context.Static = STATIC_URL
-  err := templates.ExecuteTemplate(w, template, context)
-  if err != nil {
-    log.Print("Failed to render template: ", err)
-  }
-}
-
-func StaticHandler(w http.ResponseWriter, req *http.Request) {
-  static_file := req.URL.Path[len(STATIC_URL):]
-  if len(static_file) != 0 {
-    f, err := http.Dir(STATIC_ROOT).Open(static_file)
-    if err == nil {
-      content := io.ReadSeeker(f)
-      http.ServeContent(w, req, static_file, time.Now(), content)
-      return
-    }
-  }
-  http.NotFound(w, req)
+func render(w http.ResponseWriter, template string) {
+	err := templates.ExecuteTemplate(w, template, nil)
+	if err != nil {
+		log.Print("Failed to render template: ", err)
+	}
 }
 
 func main() {
-  port := os.Getenv("PORT")
-  if len(port) == 0 {
-    port = "8081"
-  }
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8081"
+	}
 
-  log.Print("Listening on port: " + port)
+	log.Print("Listening on port: " + port)
 
-  http.HandleFunc("/", handler)
-  http.HandleFunc(STATIC_URL, StaticHandler)
-  http.ListenAndServe(":" + port, nil)
+	r := mux.NewRouter().StrictSlash(true)
+	css := http.StripPrefix("/css/", http.FileServer(http.Dir("./www/static/css/")))
+	r.PathPrefix("/css/").Handler(css)
+	fa := http.StripPrefix("/fontawesome/", http.FileServer(http.Dir("./www/static/fontawesome/")))
+	r.PathPrefix("/fontawesome/").Handler(fa)
+	f := http.StripPrefix("/fonts/", http.FileServer(http.Dir("./www/static/fonts/")))
+	r.PathPrefix("/fonts/").Handler(f)
+	img := http.StripPrefix("/img/", http.FileServer(http.Dir("./www/static/img/")))
+	r.PathPrefix("/img/").Handler(img)
+	js := http.StripPrefix("/js/", http.FileServer(http.Dir("./www/static/js/")))
+	r.PathPrefix("/js/").Handler(js)
+
+	r.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
+
+	// http.HandleFunc(STATIC_URL, StaticHandler)
 }
